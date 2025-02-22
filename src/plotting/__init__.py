c055c164cbf6f7bd448be67805788e5fb0b80850
Salvador de Haro Orenes <47396303+salvadeharo10@users.noreply.github.com>
2025-02-22 13:14:01 +0100

    Update __init__.py

--- src/plotting/__init__.py
+++ src/plotting/__init__.py
@@ -13,7 +13,7 @@ For more details about GPL-3.0: https://www.gnu.org/licenses/gpl-3.0.html
 """
 
 # Importación de módulos o clases
-from .plotting import plot_average_rewards, plot_optimal_selections
+from .plotting import plot_average_rewards, plot_optimal_selections, plot_arm_statistics, plot_optimal_selections, plot_regret
 
 # Lista de módulos o clases públicas
 __all__ = ['plot_average_rewards', 'plot_optimal_selections', 'plot_arm_statistics', 'plot_regret', 'plot_optimal_selections']
