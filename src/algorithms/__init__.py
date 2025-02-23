f6db8a35a55afe21be334080bf42920042d280d3
v4Miguel <124155033+v4Miguel@users.noreply.github.com>
2025-02-23 19:44:21 +0100

    Update __init__.py

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -15,6 +15,10 @@ For more details about GPL-3.0: https://www.gnu.org/licenses/gpl-3.0.html
 # Importación de módulos o clases
 from .algorithm import Algorithm
 from .epsilon_greedy import EpsilonGreedy
+from .UCB1 import UCB1
+from .UCB2 import UCB2
+from .softmax import Softmax
+from .gradiente_preferencias import GradientBandit
 
 # Lista de módulos o clases públicas
-__all__ = ['Algorithm', 'EpsilonGreedy']
+__all__ = ['Algorithm', 'EpsilonGreedy', 'UCB1', 'UCB2', 'Softmax', 'GradientBandit']
