0504477b0e541a59bd7e01c3872cb3340898c62c
v4Miguel <124155033+v4Miguel@users.noreply.github.com>
2025-02-23 19:54:38 +0100

    Update plotting.py

--- src/plotting/plotting.py
+++ src/plotting/plotting.py
@@ -18,7 +18,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-from algorithms import Algorithm, EpsilonGreedy
+from algorithms import Algorithm, EpsilonGreedy, UCB1, UCB2, Softmax, GradientBandit
 
 
 def get_algorithm_label(algo: Algorithm) -> str:
@@ -33,6 +33,16 @@ def get_algorithm_label(algo: Algorithm) -> str:
     label = type(algo).__name__
     if isinstance(algo, EpsilonGreedy):
         label += f" (epsilon={algo.epsilon})"
+    elif isinstance(algo, UCB1):
+        label += f" (c={algo.c})"
+    elif isinstance(algo, UCB2):
+        label += f" (parametro={algo.parametro})"
+    elif isinstance(algo, Softmax):
+        label += f" (parametro={algo.parametro})"
+    elif isinstance(algo, GradientBandit):
+        label += f" (parametro={algo.parametro})"
+    
+    
     # elif isinstance(algo, OtroAlgoritmo):
     #     label += f" (parametro={algo.parametro})"
     # Añadir más condiciones para otros algoritmos aquí
